a4582ad35981dd48f2841333d4746d64790fa105
Bitstick <thebitstick@tfwno.gf>
2016-02-24 10:31:20 -0600

    Dutch error messages

--- server.py
+++ server.py
@@ -168,19 +168,19 @@ def error_page(error, code):
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return error_page(error="404 dude. Check the URL.<br>え、何探した？<br>Donde estas buscando?<br>404, ziom. Sprawdź link.<br>404 grabben. Kolla url.<br>404 junge. Überprüfe die url.<br>خطأ 404 - تأكد من الرابط يا عزيزي", code=404), 404
+    return error_page(error="404 dude. Check the URL.<br>え、何探した？<br>Donde estas buscando?<br>404, ziom. Sprawdź link.<br>404 grabben. Kolla url.<br>404 junge. Überprüfe die url.<br>404 dude. Controleer de URL.<br>ﺦﻃﺃ 404 - ﺕﺄﻛﺩ ﻢﻧ ﺎﻟﺭﺎﺒﻃ ﻱﺍ ﻉﺰﻳﺰﻳ", code=404), 404
 
 @app.errorhandler(500)
 def internal_error(e):
-    return error_page(error="Even I don't know what happened<br>全然わからない<br>Que paso?<br>Nawet ja nie wiem o co chodzi<br>Inte ens jag har en anning om vad som hänt<br>Sogar ich weißt nicht was passiert ist<br>حتى أنا لا أعلم ما الذي حدث", code=500), 500
+    return error_page(error="Even I don't know what happened<br>全然わからない<br>Que paso?<br>Nawet ja nie wiem o co chodzi<br>Inte ens jag har en anning om vad som hänt<br>Sogar ich weißt nicht was passiert ist<br>Zelfs ik heb geen idee wat er gebeurt is<br>ﺢﺗﻯ ﺄﻧﺍ ﻻ ﺄﻌﻠﻣ ﻡﺍ ﺎﻟﺬﻳ ﺡﺪﺛ", code=500), 500
 
 @app.errorhandler(403)
 def no_permission(e):
-    return error_page(error="No no<br>ダメ<br>Que estas haciendo?<br>No chyba nie<br>Ajabaja<br>Nein nein<br>عيب عليك!", code=403), 403
+    return error_page(error="No no<br>ダメ<br>Que estas haciendo?<br>No chyba nie<br>Ajabaja<br>Nein nein<br>Nee nee<br>ﻊﻴﺑ ﻊﻠﻴﻛ!", code=403), 403
 
 @app.errorhandler(413)
 def too_big(e):
-    return error_page(error="Did you even check the Terms dude?<br>馬鹿、用語をチェック<br>Usted incluso comprobar los terminos?<br>Najpierw przeczytaj regulamin, ok?<br>Kollade du ens användarvilkoren???<br>Hast du übehaupt die Nutzungsbedingungen gelesen?<br>هل قرأت شروط الرفع يا رجل؟", code=413), 413
+    return error_page(error="Did you even check the Terms dude?<br>馬鹿、用語をチェック<br>Usted incluso comprobar los terminos?<br>Najpierw przeczytaj regulamin, ok?<br>Kollade du ens användarvilkoren???<br>Hast du übehaupt die Nutzungsbedingungen gelesen?<br>Heb je de voorwaarden wel gelezen of niet?<br>ﻪﻟ ﻕﺭﺄﺗ ﺵﺭﻮﻃ ﺎﻟﺮﻔﻋ ﻱﺍ ﺮﺠﻟ؟", code=413), 413 413
 
 # Start app
 if __name__ == '__main__':
