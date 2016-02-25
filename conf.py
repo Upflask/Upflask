c01b88129010d2a37ddb5994aefba85a564473d4
Bitstick <thebitstick@tfwno.gf>
2016-02-25 14:16:13 -0600

    Threading and Port comments with better grammar

--- conf.py
+++ conf.py
@@ -1,17 +1,18 @@
 config = dict()
 
-# Recommended to be set to 127.0.0.1 for production, and then setup Nginx to this URL
-# For debugging, feel free to use whatever host you want ("0.0.0.0")
+# Recommended to be set to 127.0.0.1 for production, and then setup Nginx to this URL.
+# For debugging, feel free to use whatever host you want ("0.0.0.0").
+# Set "PORT" to any http port you desire.
 config["HOST"] = "127.0.0.1"
 config["PORT"] = 5000
 
-# Enable for debugging
+# Enable for debugging.
 config["DEBUG"] = False
 
-# Not sure if it works AT ALL, but if it does...
+# Threading with just Python, use Gunicorn or uWSGI for multi-threading.
 config["THREADED"] = True
 
-# Site info displayed to the user
+# Site info displayed to the user.
 config["SITE_DATA"] = {
   "title": "Upflask"
 }
